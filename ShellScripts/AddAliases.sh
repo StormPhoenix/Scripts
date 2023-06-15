1b47900a08fce3353f3f6786a65df243d2580475
Cheng Wang <stormphoenix.hzau@hotmail.com>
2023-06-15 19:42:40 +0800

    Add Shell Script.

---
+++ ShellScripts/AddAliases.sh
@@ -0,0 +1,5 @@
+echo "alias tm='tmux'" >> $1
+echo "alias tml='tmux list-session'" >> $1
+echo "alias tma='tmux attach -t'" >> $1
+echo "alias tmk='tmux kill-session -t'" >> $1
+echo "alias vim='nvim'" >> $1
