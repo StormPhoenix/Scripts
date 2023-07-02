4084a807ae14583bd5546dea47a71ccf752a1d30
Cheng Wang <stormphoenix.hzau@hotmail.com>
2023-07-03 00:10:56 +0800

    修改 Docker 挂载目录。

--- DockerShell/prepare_local_dir.sh
+++ DockerShell/prepare_local_dir.sh
@@ -2,11 +2,13 @@
 
 ./build_halo_jar.sh
 
-mkdir ~/myblog
-chmod 755 ~/myblog
-cp ./halo-1.6.0-SNAPSHOT.jar ~/myblog/
+mkdir /myblog
+chmod 755 /myblog
+cp ./halo-1.6.0-SNAPSHOT.jar /myblog/
+cp ./halo-backup.zip /myblog/
 
-cd ~
+cd /myblog
+rm -rf /myblog/.halo
 unzip ./halo-backup.zip && rm halo-backup.zip
 
 
--- DockerShell/start_docker_background.sh
+++ DockerShell/start_docker_background.sh
@@ -1,2 +1,2 @@
-docker run -itd -p 8000-8100:8000-8100 --name webserver -v ~:/root/ stormphoenix/webserver /bin/bash
+docker run -itd -p 8000-8100:8000-8100 --name webserver -v /myblog:/root/myblog -v /myblog/.halo:/root/.halo stormphoenix/webserver /bin/bash
 
