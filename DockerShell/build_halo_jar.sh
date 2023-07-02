da5a3c9a647bc209e34bf5070f61cb94d98e0518
Cheng Wang <stormphoenix.hzau@hotmail.com>
2023-07-02 21:10:17 +0800

    添加 halo 1.6 自动构建脚本.

---
+++ DockerShell/build_halo_jar.sh
@@ -0,0 +1,12 @@
+CRTDIR=$(pwd)
+cd ~
+mkdir tmp_halo_build
+cd ./tmp_halo_build
+
+git clone https://github.com/halo-dev/halo.git
+cd ./halo
+git checkout -b release-1.6 origin/release-1.6
+./gradlew clean build -x check
+
+cp ./build/libs/halo-1.6.0-SNAPSHOT.jar ${CRTDIR}/
+cd ${CRTDIR}
