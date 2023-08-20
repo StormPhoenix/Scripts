472111d5a6759cd16da52dd6ee57c3a2262e7714
ubuntu <ubuntu@localhost.localdomain>
2023-08-20 18:18:54 +0800

    Update.

--- DockerShell/MediaWiki/create_mediawiki-server.sh
+++ DockerShell/MediaWiki/create_mediawiki-server.sh
@@ -1,1 +1,1 @@
-docker run -d -p 2080:80 --name mediawiki-server --network stormphoenix-net --privileged=true -v "/mediawiki/":/var/www/html mediawiki:latest
+docker run -d -p 2080:80 --name mediawiki-server --network stormphoenix-net --privileged=true -v "/mediawiki/":/data mediawiki:latest
--- DockerShell/MySQL/create_database-server.sh
+++ DockerShell/MySQL/create_database-server.sh
@@ -1,1 +1,3 @@
 docker run -d --name database-server -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --network stormphoenix-net -v /data/mysql-server:/root/Workspace stormphoenix/mysql:latest
+cp ./dump_database.sh /data/mysql-server/
+cp ./restore_database.sh /data/mysql-server/
