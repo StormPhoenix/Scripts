4d0bd6ea018d151e7db9b6c5b51d2e68804dbe4b
stormphoenix.server <stormphoenix.hzau@hotmail.com>
2023-08-23 20:24:34 +0800

    Update.

--- DockerShell/MySQL/create_database-server.sh
+++ DockerShell/MySQL/create_database-server.sh
@@ -1,3 +1,2 @@
+mkdir -p /data/mysql-server
 docker run -d --name database-server -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --network stormphoenix-net -v /data/mysql-server:/root/Workspace stormphoenix/mysql:latest
-cp ./dump_database.sh /data/mysql-server/
-cp ./restore_database.sh /data/mysql-server/
--- DockerShell/MySQL/create_wordpress-server.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 1080:80 --name wordpress-server --network stormphoenix-net -v "/data/wordpress/www/html":/var/www/html/ wordpress
